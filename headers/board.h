26d7a94c2160c0580e44c2c41efc27734b0ccc48
shdehghan <shobeir.dehghan82@gmail.com>
2022-05-25 17:04:58 +0430

    board header

---
+++ headers/board.h
@@ -0,0 +1,37 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include <SFML/Graphics.hpp>
+#include "user.h"
+#include "data.h"
+#include "cell.h"
+#include "setting.h"
+#include "helper.h"
+class Board
+{
+    public:
+        Board(sf::RenderWindow* _window);
+        sf::RenderWindow* window;
+        Cells cells;
+        User* user_w;
+        User* user_b;
+        User* curr_user;
+        sf::Font font;
+        bool end;
+        sf::Text status_text;
+
+        void init();
+        void draw();
+        void run();
+        void mouse_clicked(const sf::Vector2i& position);
+        void cell_empty_clicked(int row, int column);
+        void put_wb_in_cell(int row, int column);
+        void check_end();
+        void update_status_text();
+
+
+    protected:
+
+    private:
+};
+
+#endif
