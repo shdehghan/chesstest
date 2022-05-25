870d2fdf9fa8006fcad5434ebcab15b6c2f9502d
shdehghan <shobeir.dehghan82@gmail.com>
2022-05-25 17:17:34 +0430

    setting.cpp

---
+++ src/setting.cpp
@@ -0,0 +1,7 @@
+#include "setting.h"
+
+int setting::cell_size = 100;
+int setting::cell_offset = 20;
+sf::Color setting::cell_color_black = sf::Color(127,85,57);
+sf::Color setting::cell_color_white = sf::Color(221,184,146);
+int setting::line_space = 20;
